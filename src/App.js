55ba9a0c13d2ab24e234110c8db96100ae9e104d
Xavier Raffin <xraffin@amazon.com>
2020-05-04 15:28:36 -0700

    Fizz base version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Amplify from 'aws-amplify';
+import { AmplifyAuthenticator, AmplifySignOut, AmplifySignIn, AmplifySignUp, AmplifyButton } from '@aws-amplify/ui-react';
+import { I18n } from '@aws-amplify/core';
+import { strings } from './strings';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
+
+I18n.putVocabularies(strings);
+
+// See doc for customization here: https://docs.amplify.aws/ui/auth/authenticator/q/framework/react#slots
+
+class App extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = { lang: "en" };
+    I18n.setLanguage("en");
+  }
+
+  toggleLang = () => {
+    if (this.state.lang === "en") {
+      I18n.setLanguage("fr");
+      this.setState({ lang: "fr" });
+    } else {
+      I18n.setLanguage("en");
+      this.setState({ lang: "en" });
+    }
+  }
+
+  render = () => (
+    <div>
+      <AmplifyButton onClick={this.toggleLang}>langue {this.state.lang}</AmplifyButton>
+      <AmplifyAuthenticator usernameAlias="email">
+        <AmplifySignIn
+          usernameAlias="email"
+          headerText={I18n.get("signInHeader")}
+          slot="sign-in"
+          formFields={[
+            {
+              type: "email",
+              label: I18n.get("emailLabel"),
+              placeholder: I18n.get("emailPlaceHolder"),
+              required: true,
+            },
+            {
+              type: "password",
+              label: I18n.get("passwordLabel"),
+              placeholder: I18n.get("passwordPlaceHolder"),
+              required: true,
+            }
+          ]}></AmplifySignIn>
+        <AmplifySignUp
+          usernameAlias="email"
+          headerText={I18n.get("signUpHeader")}
+          slot="sign-up"
+          formFields={[
+            {
+              type: "email",
+              label: I18n.get("emailLabel"),
+              placeholder: I18n.get("emailPlaceHolder"),
+              required: true,
+            },
+            {
+              type: "password",
+              label: I18n.get("passwordLabel"),
+              placeholder: I18n.get("passwordPlaceHolder"),
+              required: true,
+            },
+            {
+              type: "phone_number",
+              label: I18n.get("phoneNumberLabel"),
+              placeholder: I18n.get("phoneNumberPlaceHolder"),
+              required: false,
+            }
+          ]}></AmplifySignUp>
+        <div>
+          {/* This is not really private, you should instead use the credential to load something dynamically from the backend */}
+            Private Content
+          <AmplifySignOut />
+        </div>
+      </AmplifyAuthenticator>
     </div>
   );
 }
---
+++ src/strings.js
@@ -0,0 +1,22 @@
+export const strings = {
+    en: {
+        signInHeader: "Sign in your Fizz account",
+        signUpHeader: "Create your fizz account",
+        emailLabel: "Email",
+        emailPlaceHolder: "your.email@example.com",
+        passwordLabel: "Password",
+        passwordPlaceHolder: "xxxxxxxxx",
+        phoneNumberLabel: "Phone number",
+        phoneNumberPlaceHolder: "555-555-5555"
+    },
+    fr: {
+        signInHeader: "Connexion à votre compte Fizz",
+        signUpHeader: "Créer un compte Fizz",
+        emailLabel: "Adresse de messagerie",
+        emailPlaceHolder: "votre.email@exemple.com",
+        passwordLabel: "Mot de passe",
+        passwordPlaceHolder: "xxxxxxxxx",
+        phoneNumberLabel: "Numéro de téléphone",
+        phoneNumberPlaceHolder: "555-555-5555"
+    },
+};
