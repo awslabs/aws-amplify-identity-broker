af721a5997983ae08071d62fb46c0943222e0e01
Xavier Raffin <xraffin@amazon.com>
2020-06-29 16:35:04 -0700

    Add backend API

--- amplify/backend/function/tokenfunction/src/index.js
+++ amplify/backend/function/tokenfunction/src/index.js
@@ -4,7 +4,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify('Hello from Lambda amplify/backend/function/tokenfunction/src/index.js!'),
     };
     return response;
 };
