dc9b8a61ef78df941dadb2e0b227587b359cab3c
Xavier Raffin <xraffin@amazon.com>
2020-06-29 15:53:46 -0700

    Add backend API

---
+++ amplify/backend/function/authorizefunction/src/index.js
@@ -0,0 +1,48 @@
+function handlePKCE(event) {
+    return {
+        statusCode: 200,
+        body: JSON.stringify("PKCE Flow"),
+    };
+}
+
+function handleImplicit(event) {
+    var client_id = event.queryStringParameters.client_id
+    var redirect_url = event.queryStringParameters.redirect_url
+    if (client_id === undefined || redirect_url === undefined) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify("Required parameters are missing"),
+        };
+    }
+    // Can validate client_id and redirect_url agaisnt db here
+
+    return { //redirect to login page
+        statusCode: 301,
+        headers: {
+            Location: '/',
+        }
+    };
+}
+
+exports.handler = async (event) => {
+    if (!(event && event.queryStringParameters)) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify("Required parameters are missing")
+        };
+    }
+
+    switch (event.queryStringParameters.response_type) {
+        case ('code'):
+            return handlePKCE(event);
+
+        case ('id_token'):
+            return handleImplicit(event);
+
+        default:
+            return {
+                statusCode: 400,
+                body: JSON.stringify("Response type is missing or invalid"),
+            };
+    }
+};
---
+++ amplify/backend/function/brokerauthCustomMessage/src/custom.js
@@ -0,0 +1,27 @@
+var providerName = "Unamed Service";
+exports.handler = (event, context, callback) => {
+  console.log(event);
+  // Identify why was this function invoked
+  if (event.triggerSource === "CustomMessage_SignUp") {
+    let lang = event.request.userAttributes["locale"] // Acesss the event data of custom user Attribute lang
+    console.log("Lang is " + lang);
+
+    // Ensure that your message contains event.request.codeParameter. This is the placeholder for code that will be sent
+    // French
+    if (lang == "fr") {
+      event.response.smsMessage = "Bienvenue à " + providerName + ". Votre code de confirmation est " + event.request.codeParameter;
+      event.response.emailSubject = "Bienvenue à " + providerName;
+      event.response.emailMessage = "Merci pour votre inscription. " + event.request.codeParameter + " est votre code de vérification";
+    }
+    //English
+    else {
+      event.response.smsMessage = "Welcome to " + providerName + ". Your confirmation code is " + event.request.codeParameter;
+      event.response.emailSubject = "Welcome to " + providerName;
+      event.response.emailMessage = "Thank you for signing up. " + event.request.codeParameter + " is your verification code";
+    }
+  }
+  // Create custom message for other events
+
+  // Return to Amazon Cognito
+  callback(null, event);
+};
