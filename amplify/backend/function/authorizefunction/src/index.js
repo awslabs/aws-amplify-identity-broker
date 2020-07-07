788c145842b445627d5f07cc215da7bf36de355b
liamaws <67014943+liamaws@users.noreply.github.com>
2020-07-07 18:18:28 -0400

    Update authorize function

--- amplify/backend/function/authorizefunction/src/index.js
+++ amplify/backend/function/authorizefunction/src/index.js
@@ -6,6 +6,7 @@
 Amplify Params - DO NOT EDIT */
 const AWS = require('aws-sdk');
 const { v4: uuidv4 } = require('uuid');
+const CODE_LIFE = 900000;
 
 var docClient = new AWS.DynamoDB.DocumentClient();
 var tableName = process.env.STORAGE_BACKENDDYNAMO_NAME;
@@ -14,6 +15,7 @@ async function handlePKCE(event) {
     var client_id = event.queryStringParameters.client_id;
     var redirect_url = event.queryStringParameters.redirect_url;
     var code_challenge = event.queryStringParameters.code_challenge;
+    var code_challenge_method = event.queryStringParameters.code_challenge_method;
     if (client_id === undefined || redirect_url === undefined || code_challenge === undefined) {
         return {
             statusCode: 400,
@@ -22,11 +24,15 @@ async function handlePKCE(event) {
     }
 
     const authorizationCode = uuidv4();
+    const codeExpiry = Date.now() + CODE_LIFE;
     var params = {
         TableName: tableName,
         Item: {
+            authorization_code: authorizationCode,
             code_challenge: code_challenge,
-            authorization_code: authorizationCode
+            client_id: client_id,
+            redirect_url: redirect_url,
+            code_expiry: codeExpiry
         }
     };
 
@@ -37,9 +43,11 @@ async function handlePKCE(event) {
         console.error(error);
     }
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify("PKCE Flow"),
+    return { //redirect to login page
+        statusCode: 301,
+        headers: {
+            Location: '/?client_id=' + client_id + '&redirect_url=' + redirect_url + '&code_challenge=' + code_challenge,
+        }
     };
 }
 
