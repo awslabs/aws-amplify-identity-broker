2620029aa0f997f6fa12c5a680edc62f89525221
liamaws <67014943+liamaws@users.noreply.github.com>
2020-07-07 10:50:23 -0400

    add insert to dynamodb table

--- amplify/backend/function/authorizefunction/src/index.js
+++ amplify/backend/function/authorizefunction/src/index.js
@@ -4,14 +4,46 @@
 	STORAGE_BACKENDDYNAMO_ARN
 	STORAGE_BACKENDDYNAMO_NAME
 Amplify Params - DO NOT EDIT */
-function handlePKCE(event) {
+const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+var tableName = process.env.STORAGE_BACKENDDYNAMO_NAME
+
+async function handlePKCE(event) {
+    var client_id = event.queryStringParameters.client_id
+    var redirect_url = event.queryStringParameters.redirect_url
+    var code_challenge = event.queryStringParameters.code_challenge
+    if (client_id === undefined || redirect_url === undefined || code_challenge === undefined) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify("Required parameters are missing"),
+        };
+    }
+
+    const authorizationCode = uuidv4();
+    var params = {
+        TableName: tableName,
+        Item: {
+            code_challenge: code_challenge,
+            authorization_code: authorizationCode
+        }
+    };
+
+    try {
+        var result = await docClient.put(params).promise()
+        console.log(JSON.stringify(result))
+    } catch (error) {
+        console.error(error)
+    }
+
     return {
         statusCode: 200,
         body: JSON.stringify("PKCE Flow"),
     };
 }
 
-function handleImplicit(event) {
+async function handleImplicit(event) {
     var client_id = event.queryStringParameters.client_id
     var redirect_url = event.queryStringParameters.redirect_url
     if (client_id === undefined || redirect_url === undefined) {
