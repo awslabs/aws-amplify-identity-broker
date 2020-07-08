9f8873856da39a93c5bcae80f3e75fff4e6a9113
liamaws <67014943+liamaws@users.noreply.github.com>
2020-07-08 09:22:55 -0400

    Rename code table and create client table

--- amplify/backend/function/authorizefunction/src/index.js
+++ amplify/backend/function/authorizefunction/src/index.js
@@ -1,32 +1,64 @@
 /* Amplify Params - DO NOT EDIT
 	ENV
 	REGION
-	STORAGE_BACKENDDYNAMO_ARN
-	STORAGE_BACKENDDYNAMO_NAME
+	STORAGE_CLIENTSTABLE_ARN
+	STORAGE_CLIENTSTABLE_NAME
+	STORAGE_CODESTABLE_ARN
+	STORAGE_CODESTABLE_NAME
 Amplify Params - DO NOT EDIT */
 const AWS = require('aws-sdk');
 const { v4: uuidv4 } = require('uuid');
 const CODE_LIFE = 900000;
 
 var docClient = new AWS.DynamoDB.DocumentClient();
-var tableName = process.env.STORAGE_BACKENDDYNAMO_NAME;
+var codesTableName = process.env.STORAGE_CODESTABLE_NAME
+var clientsTableName = process.env.STORAGE_CLIENTSTABLE_NAME
+
+async function verifyClient(client_id, redirect_url) {
+    var data;
+    var params = {
+        TableName: clientsTableName,
+        Key: {
+            client_id: client_id
+        }
+    };
+    try {
+        data = await docClient.get(params).promise();
+    } catch (error) {
+        console.error(error);
+    }
+    if (data.Item && (data.Item.redirect_url === redirect_url)) {
+        return true;
+    }
+    return false;
+}
+
 
 async function handlePKCE(event) {
     var client_id = event.queryStringParameters.client_id;
     var redirect_url = event.queryStringParameters.redirect_url;
     var code_challenge = event.queryStringParameters.code_challenge;
     var code_challenge_method = event.queryStringParameters.code_challenge_method;
-    if (client_id === undefined || redirect_url === undefined || code_challenge === undefined) {
+    if (client_id === undefined || redirect_url === undefined || code_challenge === undefined || code_challenge_method === undefined) {
         return {
             statusCode: 400,
             body: JSON.stringify("Required parameters are missing"),
         };
     }
 
+    // Code to verify client. Leaving commented for now
+    // var validClient = await verifyClient(client_id, redirect_url);
+    // if (!validClient) {
+    //     return {
+    //         statusCode: 400,
+    //         body: JSON.stringify("Invalid Client"),
+    //     };
+    // }
+
     const authorizationCode = uuidv4();
     const codeExpiry = Date.now() + CODE_LIFE;
     var params = {
-        TableName: tableName,
+        TableName: codesTableName,
         Item: {
             authorization_code: authorizationCode,
             code_challenge: code_challenge,
@@ -60,7 +92,15 @@ async function handleImplicit(event) {
             body: JSON.stringify("Required parameters are missing"),
         };
     }
-    // Can validate client_id and redirect_url agaisnt db here
+
+    // Code to verify client. Leaving commented for now
+    // var validClient = await verifyClient(client_id, redirect_url);
+    // if (!validClient) {
+    //     return {
+    //         statusCode: 400,
+    //         body: JSON.stringify("Invalid Client"),
+    //     };
+    // }
 
     return { //redirect to login page
         statusCode: 301,
