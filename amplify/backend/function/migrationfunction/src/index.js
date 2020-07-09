acc7754f5036b7cc0ea04dcdb7d45a0c066351d8
Xavier Raffin <xraffin@amazon.com>
2020-07-09 15:42:45 -0700

    Add migration function

---
+++ amplify/backend/function/migrationfunction/src/index.js
@@ -0,0 +1,14 @@
+/* Amplify Params - DO NOT EDIT
+	AUTH_BROKERAUTH_USERPOOLID
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
---
+++ amplify/xraffin/.temp/#current-cloud-backend/function/storagefunction/src/index.js
@@ -0,0 +1,10 @@
+
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda amplify/backend/function/storagefunction/src/index.js!'),
+    };
+    return response;
+};
