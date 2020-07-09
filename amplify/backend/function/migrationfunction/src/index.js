fda63dfbf2f04638ad28b045f5590bd2e32f5f4a
Xavier Raffin <xraffin@amazon.com>
2020-07-09 15:57:41 -0700

    Add migration function

--- amplify/backend/function/migrationfunction/src/index.js
+++ amplify/backend/function/migrationfunction/src/index.js
@@ -1,10 +1,13 @@
 /* Amplify Params - DO NOT EDIT
-	AUTH_BROKERAUTH_USERPOOLID
+    AUTH_BROKERAUTH_USERPOOLID
+    AUTH_BROKERAUTH_APPCLIENTID
 	ENV
 	REGION
 Amplify Params - DO NOT EDIT */
 
 exports.handler = async (event) => {
+    console.log("UserPoolID = " + process.env.AUTH_BROKERAUTH_USERPOOLID);
+    console.log("AppClientID = " + process.env.AUTH_BROKERAUTH_APPCLIENTID);
     // TODO implement
     const response = {
         statusCode: 200,
