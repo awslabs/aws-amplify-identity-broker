ba5f12ebcbbeaf462f5548f96c721dc6866dc834
liamaws <67014943+liamaws@users.noreply.github.com>
2020-07-10 11:49:09 -0400

    give storage function dynamo access

--- amplify/backend/function/amplifyIdentityBrokerStorage/src/index.js
+++ amplify/backend/function/amplifyIdentityBrokerStorage/src/index.js
@@ -1,10 +1,25 @@
+/* Amplify Params - DO NOT EDIT
+	ENV
+	REGION
+	STORAGE_AMPLIFYIDENTITYBROKERCODESTABLE_ARN
+	STORAGE_AMPLIFYIDENTITYBROKERCODESTABLE_NAME
+Amplify Params - DO NOT EDIT */
 
+const AWS = require('aws-sdk');
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+var codesTableName = process.env.STORAGE_AMPLIFYIDENTITYBROKERCODESTABLE_NAME
 
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
+    console.log(event);
+    if (event.body) {
+        console.log(event.body);
+        var json = JSON.parse(event.body);
+        console.log(json);
+    }
+
+    return {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda amplify/backend/function/amplifyIdentityBrokerStorage/src/index.js!'),
     };
-    return response;
 };
