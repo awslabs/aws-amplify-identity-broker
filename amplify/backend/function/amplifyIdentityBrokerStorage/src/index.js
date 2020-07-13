bb11152de4d62e0d810f2462c5c5381479fb6f8f
liamaws <67014943+liamaws@users.noreply.github.com>
2020-07-13 10:33:26 -0400

    Add storage function logic

--- amplify/backend/function/amplifyIdentityBrokerStorage/src/index.js
+++ amplify/backend/function/amplifyIdentityBrokerStorage/src/index.js
@@ -8,18 +8,52 @@ Amplify Params - DO NOT EDIT */
 const AWS = require('aws-sdk');
 
 var docClient = new AWS.DynamoDB.DocumentClient();
-var codesTableName = process.env.STORAGE_AMPLIFYIDENTITYBROKERCODESTABLE_NAME
+var codesTableName = process.env.STORAGE_AMPLIFYIDENTITYBROKERCODESTABLE_NAME;
 
 exports.handler = async (event) => {
-    console.log(event);
-    if (event.body) {
-        console.log(event.body);
-        var json = JSON.parse(event.body);
-        console.log(json);
+    if (!event.body) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify('Empty body'),
+        };
+    }
+
+    var jsonBody = JSON.parse(event.body);
+    var authorization_code = jsonBody.authorization_code;
+    var id_token = jsonBody.id_token;
+    var access_token = jsonBody.access_token;
+
+    if (authorization_code === undefined || id_token === undefined || access_token === undefined) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify('Body missing values'),
+        };
+    }
+
+    var params = {
+        TableName: codesTableName,
+        Key: {
+            authorization_code: authorization_code
+        },
+        UpdateExpression: "SET id_token = :idt, access_token = :at",
+        ExpressionAttributeValues: {
+            ":idt": id_token,
+            ":at": access_token
+        }
+    };
+
+    try {
+        var result = await docClient.update(params).promise();
+    } catch (error) {
+        console.error(error);
+        return {
+            statusCode: 400,
+            body: error,
+        };
     }
 
     return {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda amplify/backend/function/amplifyIdentityBrokerStorage/src/index.js!'),
+        body: JSON.stringify('Inserted tokens into storage'),
     };
 };
