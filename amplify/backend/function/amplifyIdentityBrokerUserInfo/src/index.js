5fa41f0179ee298490310d465fd3885ae4771618
liamaws <67014943+liamaws@users.noreply.github.com>
2020-07-17 15:36:15 -0400

    Add license info

--- amplify/backend/function/amplifyIdentityBrokerUserInfo/src/index.js
+++ amplify/backend/function/amplifyIdentityBrokerUserInfo/src/index.js
@@ -1,5 +1,18 @@
+/* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ 
+Licensed under the Apache License, Version 2.0 (the "License").
+You may not use this file except in compliance with the License.
+A copy of the License is located at
+ 
+    http://www.apache.org/licenses/LICENSE-2.0
+ 
+or in the "license" file accompanying this file. This file is distributed 
+on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+express or implied. See the License for the specific language governing 
+permissions and limitations under the License. */
+
 const AWS = require('aws-sdk');
-var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
+var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider(); // Investigate further to see how this works
 
 exports.handler = async (event) => {
     // Reference here https://docs.aws.amazon.com/cognito/latest/developerguide/userinfo-endpoint.html
