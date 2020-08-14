7439261b1e39d193be006d46f3e5e174ddcd55ea
Vo <kevnvo@a483e743386a.ant.amazon.com>
2020-08-14 13:31:24 -0700

    Fix last push error

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ import jwt_decode from 'jwt-decode';
 import awsconfig from './aws-exports';
 var Config = require("Config");
 
-  Amplify.configure({...awsconfig, 
+Amplify.configure({...awsconfig, 
 	Auth: {
 		// OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
 		authenticationFlowType: Config.authenticationFlowType !== undefined ? Config.authenticationFlowType : "USER_SRP_AUTH",
@@ -244,11 +244,11 @@ class App extends React.Component {
                 },
                 {
                   //Not possible to do a dropdown menu in the amplify sigin page. I will set a request for the amplify to get this feature added.
-                  label: "Membership (Standard, Premium, Admin)",
+                  label: I18n.get("MEMBERSHIP"),
                   type: "custom:customer-type",
                   // We set the default to Standard for now, because if the user leaves the form field blank, it will send a null data type and error will be flagged.
                   // The user will override this form field if they want to set the membership other then Standard.
-                  value: "Standard",
+                  value: I18n.get("MEMBERSHIP_VALUE"),
                   required: false,
                 }
               ]}></AmplifySignUp>
