ee7dea5364fb1293a87c4debcff72afc14245c06
Vo <kevnvo@a483e743386a.ant.amazon.com>
2020-08-14 16:18:08 -0700

    Upadted new Licenses

--- amplify/backend/function/amplifyIdentityBrokerCustomMessage/src/index.js
+++ amplify/backend/function/amplifyIdentityBrokerCustomMessage/src/index.js
@@ -1,18 +1,14 @@
-/* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- 
-Licensed under the Apache License, Version 2.0 (the "License").
-You may not use this file except in compliance with the License.
-A copy of the License is located at
- 
-    http://www.apache.org/licenses/LICENSE-2.0
- 
-or in the "license" file accompanying this file. This file is distributed 
-on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
-express or implied. See the License for the specific language governing 
-permissions and limitations under the License. */
+/*
+  * Copyright Amazon.com, Inc. and its affiliates. All Rights Reserved.
+  * SPDX-License-Identifier: MIT
+  *
+  * Licensed under the MIT License. See the LICENSE accompanying this file
+  * for the specific language governing permissions and limitations under
+  * the License.
+  */
 
 /*
-  this file will loop through all js modules which are uploaded to the lambda resource,
+  This file will loop through all js modules which are uploaded to the lambda resource,
   provided that the file names (without extension) are included in the "MODULES" env variable.
   "MODULES" is a comma-delimmited string.
 */
