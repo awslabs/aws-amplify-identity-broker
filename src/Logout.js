5f61a6e8da073c8c6b2695d33eec8e00188ae99c
liamaws <67014943+liamaws@users.noreply.github.com>
2020-08-20 10:27:10 -0400

    add call to hosted ui /logout endpoint

--- src/Logout.js
+++ src/Logout.js
@@ -10,38 +10,74 @@
 import React from 'react';
 import { Auth } from 'aws-amplify';
 import { eraseCookie } from './helpers'
+import awsconfig from './aws-exports';
+var Config = require("Config");
 
 class Logout extends React.Component {
     componentDidMount() {
-        // Sign out the user and erase the id_token cookie
-        Auth.signOut();
-        eraseCookie("id_token");
-        eraseCookie("access_token");
+        var redirectInfo = localStorage.getItem('redirectInfo');
 
-        // Accept requests according to https://docs.aws.amazon.com/cognito/latest/developerguide/logout-endpoint.html
-        let queryStringParams = new URLSearchParams(window.location.search);
-        let clientID = queryStringParams.get('client_id');
-        let logoutURI = queryStringParams.get('logout_uri');
-        let redirectURI = queryStringParams.get('redirect_uri');
-        let responseType = queryStringParams.get('response_type');
-        let codeChallenge = queryStringParams.get('code_challenge');
-        let codeChallengeMethod = queryStringParams.get('code_challenge_method');
+        if (redirectInfo) { // For handling a redirect back from the Cognito Hosted UI
+            localStorage.removeItem('redirectInfo');
+            var rdJSON = JSON.parse(redirectInfo);
+            if (rdJSON['clientID'] && rdJSON['logoutURI']) { // Redirect back to client
+                // TODO: Check clientID and logoutURI agaisnt entry in dynamoDB
+                window.location.assign(rdJSON['logoutURI']);
+            }
+            else if (rdJSON['responseType'] === "id_token" && rdJSON['clientID'] && rdJSON['redirectURI']) { // Call authorize endpoint to start implicit flow
+                let authorizeEndpointPath = '/oauth2/authorize/?response_type=' + rdJSON['responseType']
+                    + "&client_id=" + rdJSON['clientID'] + "&redirect_uri=" + rdJSON['redirectURI'];
+                window.location.href = authorizeEndpointPath;
+            }
+            else if (rdJSON['responseType'] && rdJSON['clientID'] && rdJSON['redirectURI'] && rdJSON['codeChallenge'] && rdJSON['codeChallengeMethod']) { // Call authorize endpoint to start PKCE flow
+                let authorizeEndpointPath = '/oauth2/authorize/?response_type=' + rdJSON['responseType'] + "&client_id=" + rdJSON['clientID']
+                    + "&redirect_uri=" + rdJSON['redirectURI'] + "&code_challenge=" + rdJSON['codeChallenge']
+                    + "&code_challenge_method=" + rdJSON['codeChallengeMethod'];
+                window.location.href = authorizeEndpointPath;
+            }
+            else { // Default to redirecting to the broker login page
+                window.location.href = '/';
+            }
 
-        if (clientID && logoutURI) { // Redirect back to client
-            // TODO: Check clientID and logoutURI agaisnt entry in dynamoDB
-            window.location.assign(logoutURI);
         }
-        else if (responseType === "id_token" && clientID && redirectURI) { // Call authorize endpoint to start implicit flow
-            let authorizeEndpointPath = '/oauth2/authorize/?response_type=' + responseType + "&client_id=" + clientID + "&redirect_uri=" + redirectURI;
-            window.location.href = authorizeEndpointPath;
-        }
-        else if (responseType === "code" && clientID && redirectURI && codeChallenge && codeChallengeMethod) { // Call authorize endpoint to start PKCE flow
-            let authorizeEndpointPath = '/oauth2/authorize/?response_type=' + responseType + "&client_id=" + clientID + "&redirect_uri=" + redirectURI
-                + "&code_challenge=" + codeChallenge + "&code_challenge_method=" + codeChallengeMethod;
-            window.location.href = authorizeEndpointPath;
-        }
-        else { // Default to redirecting to the broker login page
-            window.location.href = '/';
+        else { // If the logout endpoint is being called before the Hosted UI logout endpoint has been called
+            // Sign out the user and erase the token cookies
+            Auth.signOut();
+            eraseCookie("id_token");
+            eraseCookie("access_token");
+
+            // Accept requests according to https://docs.aws.amazon.com/cognito/latest/developerguide/logout-endpoint.html
+            let queryStringParams = new URLSearchParams(window.location.search);
+            let clientID = queryStringParams.get('client_id');
+            let logoutURI = queryStringParams.get('logout_uri');
+            let redirectURI = queryStringParams.get('redirect_uri');
+            let responseType = queryStringParams.get('response_type');
+            let codeChallenge = queryStringParams.get('code_challenge');
+            let codeChallengeMethod = queryStringParams.get('code_challenge_method');
+
+            // Store the redirect info in local storage before calling the Cognito Hosted UI to logout
+            var redirectObject;
+            if (clientID && logoutURI) { // For redirect to client
+                redirectObject = { 'clientID': clientID, 'logoutURI': logoutURI };
+            }
+            else if (responseType === "id_token" && clientID && redirectURI) { // For implicit flow
+                redirectObject = { 'clientID': clientID, 'responseType': responseType, 'redirectURI': redirectURI };
+            }
+            else if (responseType === "code" && clientID && redirectURI && codeChallenge && codeChallengeMethod) { // For PKCE flow
+                redirectObject = { 'clientID': clientID, 'responseType': responseType, 'redirectURI': redirectURI, 'codeChallenge': codeChallenge, 'codeChallengeMethod': codeChallengeMethod };
+            }
+            else {
+                redirectObject = { 'redirect': 'broker' };
+            }
+            localStorage.setItem('redirectInfo', JSON.stringify(redirectObject));
+
+            // Call Cognito Hosted UI /logout endpoint
+            const hostedUILogoutEndpoint = new URL(Config.hostedUIUrl + '/logout');
+            hostedUILogoutEndpoint.search = new URLSearchParams({
+                client_id: awsconfig.aws_user_pools_web_client_id,
+                logout_uri: window.location.origin + '/logout'
+            });
+            window.location.assign(hostedUILogoutEndpoint.href);
         }
     }
 
