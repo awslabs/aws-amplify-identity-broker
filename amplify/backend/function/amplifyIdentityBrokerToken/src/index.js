f861af285a28f44c4fe32f3451ab7a506753d540
liamaws <67014943+liamaws@users.noreply.github.com>
2020-08-28 17:06:25 -0400

    add refresh token response to token endpoint

--- amplify/backend/function/amplifyIdentityBrokerToken/src/index.js
+++ amplify/backend/function/amplifyIdentityBrokerToken/src/index.js
@@ -19,6 +19,7 @@ const crypto = require('crypto');
 const qs = require('querystring');
 
 var docClient = new AWS.DynamoDB.DocumentClient();
+var cognitoSP = new AWS.CognitoIdentityServiceProvider();
 var codesTableName = process.env.STORAGE_AMPLIFYIDENTITYBROKERCODESTABLE_NAME;
 
 function base64URLEncode(str) {
@@ -41,83 +42,145 @@ exports.handler = async (event) => {
     }
 
     var jsonBody = qs.parse(event.body);
+    var grant_type = jsonBody.grant_type;
     var client_id = jsonBody.client_id;
-    var authorization_code = jsonBody.authorization_code;
-    var redirect_uri = jsonBody.redirect_uri;
-    var code_verifier = jsonBody.code_verifier;
-    if (client_id === undefined || redirect_uri === undefined || authorization_code === undefined || code_verifier == undefined) {
+
+    if (client_id === undefined) {
         return {
             statusCode: 400,
-            body: JSON.stringify("Required parameters are missing"),
+            body: JSON.stringify("client_id is missing"),
         };
     }
 
-    var data;
-    var params = {
-        TableName: codesTableName,
-        Key: {
-            authorization_code: authorization_code
+    if (grant_type === "authorization_code") {
+        var authorization_code = jsonBody.authorization_code;
+        var redirect_uri = jsonBody.redirect_uri;
+        var code_verifier = jsonBody.code_verifier;
+        if (redirect_uri === undefined || authorization_code === undefined || code_verifier == undefined) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify("Required parameters are missing"),
+            };
         }
-    };
-    try {
-        data = await docClient.get(params).promise();
-    } catch (error) {
-        console.error(error);
-    }
 
-    if (data.Item === undefined) {
+        var data;
+        var params = {
+            TableName: codesTableName,
+            Key: {
+                authorization_code: authorization_code
+            }
+        };
+        try {
+            data = await docClient.get(params).promise();
+        } catch (error) {
+            console.error(error);
+        }
+
+        if (data.Item === undefined) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify('Invalid authorization code'),
+            };
+        }
+        if (data.Item.client_id != client_id) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify('Client ID does not match authorization code'),
+            };
+        }
+        if (data.Item.redirect_uri != redirect_uri) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify('Redirect uri does not match authorization code'),
+            };
+        }
+        if (Date.now() > data.Item.code_expiry) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify('Authorization code expired'),
+            };
+        }
+
+        var calculatedCodeChallenge = base64URLEncode(sha256(code_verifier));
+
+        if (calculatedCodeChallenge !== data.Item.code_challenge) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify('Code verifier does not match code challenge'),
+            };
+        }
+
+        var access_token = data.Item.access_token;
+        var id_token = data.Item.id_token;
+        var refresh_token = data.Item.refresh_token;
+
+        if (access_token === undefined || id_token === undefined || refresh_token === undefined) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify('Could not find tokens'),
+            };
+        }
+
         return {
-            statusCode: 400,
-            body: JSON.stringify('Invalid authorization code'),
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*" // Required for CORS support to work
+            },
+            body: JSON.stringify({
+                "access_token": access_token,
+                "id_token": id_token,
+                "refresh_token": refresh_token,
+                "token_type": "Bearer"
+            }),
         };
     }
-    if (data.Item.client_id != client_id) {
+    else if (grant_type === "refresh_token") {
+        var refresh_token = jsonBody.refresh_token;
+        if (refresh_token === undefined) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify("refresh_token is missing"),
+            };
+        }
+
+        var params = {
+            AuthFlow: 'REFRESH_TOKEN',
+            ClientId: client_id,
+            AuthParameters: {
+                "REFRESH_TOKEN": refresh_token
+            }
+        }
+
+        var cognitoResponse = await cognitoSP.initiateAuth(params).promise(); // Call Cognito with refresh token to get refreshed id and access tokens
+        var access_token = cognitoResponse.AuthenticationResult.AccessToken;
+        var id_token = cognitoResponse.AuthenticationResult.IdToken;
+
+        if (access_token === undefined || id_token === undefined) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify('Could not refresh tokens'),
+            };
+        }
+
         return {
-            statusCode: 400,
-            body: JSON.stringify('Client ID does not match authorization code'),
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*" // Required for CORS support to work
+            },
+            body: JSON.stringify({
+                "access_token": access_token,
+                "id_token": id_token,
+                "token_type": "Bearer"
+            }),
         };
     }
-    if (data.Item.redirect_uri != redirect_uri) {
+    else {
         return {
             statusCode: 400,
-            body: JSON.stringify('Redirect uri does not match authorization code'),
+            headers: {
+                "Access-Control-Allow-Origin": "*" // Required for CORS support to work
+            },
+            body: JSON.stringify("Invalid grant type"),
         };
     }
-    if (Date.now() > data.Item.code_expiry) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify('Authorization code expired'),
-        };
-    }
-
-    var calculatedCodeChallenge = base64URLEncode(sha256(code_verifier));
-
-    if (calculatedCodeChallenge !== data.Item.code_challenge) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify('Code verifier does not match code challenge'),
-        };
-    }
-
-    var access_token = data.Item.access_token;
-    var id_token = data.Item.id_token;
-
-    if (access_token === undefined || id_token === undefined) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify('Could not find tokens'),
-        };
-    }
-
-    return {
-        statusCode: 200,
-        headers: {
-            "Access-Control-Allow-Origin": "*" // Required for CORS support to work
-        },
-        body: JSON.stringify({
-            "access_token": access_token,
-            "id_token": id_token,
-            "token_type": "Bearer"
-        }),
-    };
 };
