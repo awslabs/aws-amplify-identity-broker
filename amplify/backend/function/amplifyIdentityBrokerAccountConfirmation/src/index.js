e260a5ab0be10f7ac6a8914452bade0b01551e25
nipun <chagarin@amazon.com>
2020-09-03 12:24:25 -0700

    Made some minor changes and cleaned up the code

--- amplify/backend/function/amplifyIdentityBrokerAccountConfirmation/src/index.js
+++ amplify/backend/function/amplifyIdentityBrokerAccountConfirmation/src/index.js
@@ -1,30 +1,30 @@
 'use strict';
 var AWS = require('aws-sdk');
 if (typeof Promise === 'undefined') {
-AWS.config.setPromisesDependency(require('bluebird'));
+    AWS.config.setPromisesDependency(require('bluebird'));
 }
 var CognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider({
     apiVersion: '2019-11-07',
     region: process.env.REGION
 });
 exports.handler = (req, context, callback) => {
-console.log(req);
+    console.log(req);
     const confirmationCode = req.code;
     const username = req.username;
     const clientId = req.clientId;
-let params = {
+    let params = {
         ClientId: clientId,
         ConfirmationCode: confirmationCode,
         Username: username
     };
-    
-console.log(params);
 
-//Validating the user
-let confirmSignUp = CognitoIdentityServiceProvider.confirmSignUp(params).promise();
+    console.log(params);
 
-//Returning the redirect url
-confirmSignUp.then(
+    //Validating the user
+    let confirmSignUp = CognitoIdentityServiceProvider.confirmSignUp(params).promise();
+
+    //Returning the redirect url
+    confirmSignUp.then(
         (data) => {
             context.succeed({
                 location: process.env.REDIRECTURL
