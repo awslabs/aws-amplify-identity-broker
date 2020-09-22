620e500e7adbae5446cb467016b423b2bdbfe79d
Xavier <xavierraffin@gmail.com>
2020-09-22 12:11:15 -0700

    Handle accountConfirmation error case

--- amplify/backend/function/amplifyIdentityBrokerAccountConfirmation/src/index.js
+++ amplify/backend/function/amplifyIdentityBrokerAccountConfirmation/src/index.js
@@ -7,38 +7,47 @@
   * the License.
   */
  
-'use strict';
-var AWS = require('aws-sdk');
-var CognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider({
-    apiVersion: '2019-11-07',
-    region: process.env.REGION
-});
-exports.handler = (req, context, callback) => {
-    console.log(req);
-    const confirmationCode = req.code;
-    const username = req.username;
-    const clientId = req.clientId;
-    let params = {
-        ClientId: clientId,
-        ConfirmationCode: confirmationCode,
-        Username: username
-    };
-
-    console.log(params);
-
-    //Validating the user
-    let confirmSignUp = CognitoIdentityServiceProvider.confirmSignUp(params).promise();
-
-    //Returning the redirect url
-    confirmSignUp.then(
-        (data) => {
-            context.succeed({
-                location: process.env.HOSTING_DOMAIN
-            });
-        }
-    ).catch(
-        (error) => {
-            callback(error.message)
-        }
-    )
-};
+ 'use strict';
+ var AWS = require('aws-sdk');
+ var CognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider({
+     apiVersion: '2019-11-07',
+     region: process.env.REGION
+ });
+ exports.handler = (req, context, callback) => {
+     console.log(req);
+     const confirmationCode = req.code;
+     const username = req.username;
+     const clientId = req.clientId;
+     let params = {
+         ClientId: clientId,
+         ConfirmationCode: confirmationCode,
+         Username: username
+     };
+ 
+     console.log(params);
+ 
+     //Validating the user
+     let confirmSignUp = CognitoIdentityServiceProvider.confirmSignUp(params).promise();
+ 
+     //Returning the redirect url
+     confirmSignUp.then(
+         (data) => {
+             context.succeed({
+                 location: process.env.HOSTING_DOMAIN
+             });
+         }
+     ).catch(
+         (error) => {
+             if(error.message === "User cannot be confirmed. Current status is CONFIRMED"){
+                 var html = `<html><head><title>Account already confirmed</title></head> 
+                             <body><b>The account ${username} has been already confirmed</b><br/>
+                             Click <a href="${process.env.HOSTING_DOMAIN}">here</a> to login.
+                             </body></html>`;
+                     
+                 context.succeed(html);
+             } else {
+                 callback(error.message);
+             }
+         }
+     )
+ };
