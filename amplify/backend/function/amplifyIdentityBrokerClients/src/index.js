dabee97243f04d477514836ddaa91e13be4bf8d9
iLyas Bakouch <ibakouch@amazon.com>
2020-10-02 14:33:54 -0400

    use promises to get data from DDB table

--- amplify/backend/function/amplifyIdentityBrokerClients/src/index.js
+++ amplify/backend/function/amplifyIdentityBrokerClients/src/index.js
@@ -1,57 +1,49 @@
 /*
-    Copyright Amazon.com, Inc. and its affiliates. All Rights Reserved.
-    SPDX-License-Identifier: MIT
-    Licensed under the MIT License. See the LICENSE accompanying this file
-    for the specific language governing permissions and limitations under
-    the License.
-*/
-
+  * Copyright Amazon.com, Inc. and its affiliates. All Rights Reserved.
+  * SPDX-License-Identifier: MIT
+  *
+  * Licensed under the MIT License. See the LICENSE accompanying this file
+  * for the specific language governing permissions and limitations under
+  * the License.
+  */
 
 /* Amplify Params - DO NOT EDIT
+    API_AMPLIFYIDENTITYBROKERAPI_APIID
+	API_AMPLIFYIDENTITYBROKERAPI_APINAME
 	ENV
-	HOSTING_S3ANDCLOUDFRONT_HOSTINGBUCKETNAME
 	REGION
 	STORAGE_AMPLIFYIDENTITYBROKERCLIENTSTABLE_ARN
 	STORAGE_AMPLIFYIDENTITYBROKERCLIENTSTABLE_NAME
 Amplify Params - DO NOT EDIT */
 
-const AWS      = require('aws-sdk')
-const dynamodb = new AWS.DynamoDB();
-
-let tableName = process.env.STORAGE_AMPLIFYIDENTITYBROKERCLIENTSTABLE_ARN;
-
-console.log("tableName: " + tableName);
-console.log("STORAGE_AMPLIFYIDENTITYBROKERCLIENTSTABLE_ARN: " + STORAGE_AMPLIFYIDENTITYBROKERCLIENTSTABLE_ARN);
-
-if (process.env.ENV && process.env.ENV !== "NONE") {
-  tableName = tableName + '-' + process.env.ENV;
-  console.debug("Table Name: " + tableName);
-}
+const AWS              = require('aws-sdk');
+const docClient        = new AWS.DynamoDB.DocumentClient();
+const clientsTableName = process.env.STORAGE_AMPLIFYIDENTITYBROKERCLIENTSTABLE_NAME;
 
 exports.handler = async (event) => {
-    let queryParams = {
-        TableName: tableName
+    let data;
+    let params = {
+        TableName: clientsTableName
+    };
+
+    console.debug("params: " + JSON.stringify(params));
+
+    try {
+        data = await docClient.scan(params).promise();
+    } catch (error) {
+        console.error(error);
     }
 
-    let response = {
-        headers: {
-            "Access-Control-Allow-Origin": "*"
-        }
-    };
-    dynamodb.scan(queryParams, (err, data) => {
-        if (err) {
-            console.error("error: " + err);
-            response.body       = JSON.stringify({error: 'Could not load clients: ' + err});
-            response.statusCode = 500;
-        } else {
-            console.debug("success");
-            response.body       = JSON.stringify(data.Items);
-            response.statusCode = 200;
-        }
+    if (data.Items) {
+        console.debug("my response:");
+        console.debug(JSON.stringify(data));
 
-        console.debug("response");
-        console.debug(JSON.stringify(response));
-
-        return response;
-    });
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data.Items),
+            headers: {
+                "Access-Control-Allow-Origin": "*"
+            }
+        };
+    }
 };
