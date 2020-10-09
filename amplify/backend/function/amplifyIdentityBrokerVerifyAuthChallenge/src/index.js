6904c29a1ea392a3acd5a4e239869c965b46ab90
Xavier <xavierraffin@gmail.com>
2020-10-08 18:40:46 -0700

    Now check username token match

--- amplify/backend/function/amplifyIdentityBrokerVerifyAuthChallenge/src/index.js
+++ amplify/backend/function/amplifyIdentityBrokerVerifyAuthChallenge/src/index.js
@@ -11,19 +11,16 @@
  var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
 
 exports.handler = async (event) => {
-    // TODO remove the logs
-    console.log(event);
-    console.log("token = " + event.request.challengeAnswer);
-    // TODO: Check the token against Cognito, and only return the token if the user is the same and the token valid
-    
     var params = {
       AccessToken: event.request.challengeAnswer
     };
     var userInfo = await cognitoidentityserviceprovider.getUser(params).promise();
     
-    console.log(userInfo);
-    //console.log("equals? = "( userInfo.Username === event.username ));
-
-    event.response.answerCorrect = true;
+    if (userInfo.Username === event.username) {
+      event.response.answerCorrect = true;
+    } else {
+      // Someone tried to get a token of someone else
+      event.response.answerCorrect = false;
+    }
     return event;
 };
