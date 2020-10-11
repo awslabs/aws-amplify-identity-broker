54b694cb4e106087aecbd9f480e24eff18361eed
iLyas <isbkch@gmail.com>
2020-10-11 15:10:46 -0400

    add breakpoints for different devices

---
+++ src/responsive-utilities/phone_breakpoint.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Breakpoint from './breakpoint';
+
+export default function PhoneBreakpoint(props) {
+    return (
+        <Breakpoint name="phone">
+        {props.children}
+        </Breakpoint>
+    );
+}
