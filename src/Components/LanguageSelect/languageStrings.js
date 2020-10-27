744423ea32d90c1019d00e0ed3d5c7d99fef0b04
nbaeumer <nbaeumer@amazon.de>
2020-10-27 11:25:40 +0100

    Fix Language Select

--- src/Components/LanguageSelect/languageStrings.js
+++ src/Components/LanguageSelect/languageStrings.js
@@ -1,34 +1,14 @@
 export const strings = {
   en: {
-    SELECT_LABEL: "Language",
-    HELPER_TEXT: "Select your preferred language",
-    EN: "English",
-    FR: "France",
-    DE: "German",
-    NL: "Dutch"   
+    SELECT_LABEL: "Language"
   },
   fr: {
-    SELECT_LABEL: "Langue",
-    HELPER_TEXT: "Sélectionnez votre langue préférée",
-    EN: "Anglais",
-    FR: "Français",
-    DE: "Allemand",
-    NL: "Néerlandais"    
+    SELECT_LABEL: "Langue"
   },
   de: {
-    SELECT_LABEL: "Sprache",
-    HELPER_TEXT: "Wähle Deine bevorzugte Sprache",
-    EN: "Englisch",
-    FR: "Französisch",
-    DE: "Deutsch",
-    NL: "Niederländisch"    
+    SELECT_LABEL: "Sprache"
   },
   nl: {
-    SELECT_LABEL: "Taal",
-    HELPER_TEXT: "Kies uw voorkeurstaal",
-    EN: "Engles",
-    FR: "Frans",
-    DE: "Duitse",
-    NL: "Nederlands"    
+    SELECT_LABEL: "Taal"
   }
 }
