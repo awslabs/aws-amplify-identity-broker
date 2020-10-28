f236dba651bd853bf810b1a1cfc8ed9d75ee9888
Xavier <xavierraffin@gmail.com>
2020-10-28 12:42:56 -0700

    Update a commented line

--- src/index.js
+++ src/index.js
@@ -79,5 +79,5 @@ ReactDOM.render(
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: https://create-react-app.dev/docs/making-a-progressive-web-app
 serviceWorker.unregister();
