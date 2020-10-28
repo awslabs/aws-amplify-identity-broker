94c7c814fa9b5b1ae1d8346827d6e829f8843de3
Xavier <xavierraffin@gmail.com>
2020-10-28 10:35:15 -0700

    Standardize indentation

--- src/index.js
+++ src/index.js
@@ -31,10 +31,7 @@ let amplifyConfig = {
 	...awsconfig,
 	Auth: {
 		// OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
-		authenticationFlowType:
-			Config.authenticationFlowType !== undefined
-				? Config.authenticationFlowType
-				: "USER_SRP_AUTH",
+		authenticationFlowType: Config.authenticationFlowType !== undefined ? Config.authenticationFlowType : "USER_SRP_AUTH",
 	},
 };
 
