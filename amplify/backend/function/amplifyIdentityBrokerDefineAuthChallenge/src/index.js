332e79c3b8ccb2062534a017fd3ab5d79cc89ccb
Xavier Raffin <xavier.raffin@gmail.com>
2021-01-19 15:58:01 -0800

    Security fix: validate challenge answer is correct on custom flow

--- amplify/backend/function/amplifyIdentityBrokerDefineAuthChallenge/src/index.js
+++ amplify/backend/function/amplifyIdentityBrokerDefineAuthChallenge/src/index.js
@@ -14,7 +14,7 @@ exports.handler = async (event) => {
 		event.response.issueTokens = false;
 		event.response.failAuthentication = false;
 		event.response.challengeName = 'CUSTOM_CHALLENGE';
-	} else if (event.request.session.length == 1 && event.request.session[0].challengeName == 'CUSTOM_CHALLENGE') {
+	} else if (event.request.session.length == 1 && event.request.session[0].challengeName == 'CUSTOM_CHALLENGE' && event.request.session[0].challengeResult == true ) {
 		// The custom challenge has been answered we retrun the token
 		event.response.issueTokens = true;
 		event.response.failAuthentication = false;
